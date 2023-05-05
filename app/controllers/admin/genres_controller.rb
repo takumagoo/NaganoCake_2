101ac754ff11d4d1c9a800d92b80454d68362b1e
takumagoo <117556435+takumagoo@users.noreply.github.com>
2023-05-05 04:51:17 +0000

    [Finish] 管理者ジャンル画面

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -7,24 +7,25 @@ class Admin::GenresController < ApplicationController
 
   def create
     # 新規登録ボタンが押されたら、データを変数に格納
-    # genre = Genre.new(genre_params)
-    # # セーブする
-    # genre.save!
-    # # 処理後のpathを指定
-    # redirect_to admin_genres_path
-
     genre = Genre.new(genre_params)
-    genre.save!
-    # リダイレクト (一応：一覧画面へ)
+    # セーブする
+    genre.save
+    # 処理後のpathを指定
     redirect_to admin_genres_path
   end
 
   def edit
-
+    @genre = Genre.find(params[:id])
   end
 
   def update
-
+    # 入力された値を受け取る変数
+    genre = Genre.find(params[:id])
+    # アップデート
+    genre.update(genre_params)
+    # アップデート後の遷移先を指定
+    redirect_to admin_genres_path
+    # redirect_to admin_genre_path
   end
 
 
