533306cdd6793cb77bf9e1be0d4af132a940ce99
takumagoo <117556435+takumagoo@users.noreply.github.com>
2023-05-08 12:44:55 +0000

    [add] admin/customer

--- db/migrate/20230503041018_devise_create_customers.rb
+++ db/migrate/20230503041018_devise_create_customers.rb
@@ -32,6 +32,22 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      # 姓
+      t.string :last_name
+      # 名
+      t.string :first_name
+      # 姓カナ
+      t.string :last_name_kana
+      # 名カナ
+      t.string :first_name_kana
+      # 郵便番号
+      t.string :postal_code
+      # 住所
+      t.string :address
+      # 電話番号
+      t.string :telephone_number
+      # 退会ステータス
+      t.boolean :is_deleted, default: false
 
       t.timestamps null: false
     end
